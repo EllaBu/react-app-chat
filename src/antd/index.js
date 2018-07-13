6ccd3bfa82e5e4e53c54e1a91f12e8ae35cd585d
buqian <abc@DESKTOP-TUADEON>
2018-07-13 10:14:41 +0800

    安装 antd-mobile

---
+++ src/antd/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Button } from 'antd-mobile'
+class Antd extends Component {
+  render () {
+    return (
+      <div>
+        <Button>Start</Button>
+      </div>
+    )
+  }
+}
+
+export default Antd;
